ceb3b55be742e88fc9129811903e5ba0fff18644
root <mo.alidev369@gmail.com>
2023-02-16 15:57:54 -0800

    /this is a commit/

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <string.h>
 /**
  * main - a void fn that prints a sentence
  * Return: 1
@@ -7,6 +8,6 @@ int main(void)
 {
 	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	int len = strlen(str);
-	write(STDERR-FILENO, str, len);
+	write(2, str, len);
 	return (1);
 }
