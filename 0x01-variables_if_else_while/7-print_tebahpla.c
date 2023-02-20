d691cd72087ef7b2627c5251a720f304ced007f5
root <mo.alidev369@gmail.com>
2023-02-19 21:47:36 -0800

    this is a commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char m = 'z';
+
+	while (m >= 'a')
+{
+	putchar(m);	/*print the current character*/
+	m++;	/*move to the next character*/
+}
+	putchar('\n');	/*print a newline character at the end*/
+
+	return (0);
+}
