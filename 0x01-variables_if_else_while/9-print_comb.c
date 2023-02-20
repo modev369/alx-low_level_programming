0d2acec9cf7454c33af78d2215689db5f3d83a62
root <mo.alidev369@gmail.com>
2023-02-19 22:13:58 -0800

    this is a commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+{
+	putchar(num + '0');	/*print the current digit*/
+
+	if (i < 9)
+{
+	putchar(',');
+	putchar(' ');
+}
+
+}
+	putchar('\n');	/*print a newline character at the end*/
+
+	return (0);
+}
