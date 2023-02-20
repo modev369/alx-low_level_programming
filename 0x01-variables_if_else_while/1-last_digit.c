4e522538088a522637b1ddee69bcc966626809c9
root <mo.alidev369@gmail.com>
2023-02-19 20:55:57 -0800

    this is a commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	last_digit = abs(n) % 10;
+	last_digit = n % 10;
 
 	printf("Last digit of %d is ", n);
 
