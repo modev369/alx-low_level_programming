e29a34fe5ec1a73263af263c059d6ee51e0850ab
root <mo.alidev369@gmail.com>
2023-02-19 22:01:07 -0800

    this is a commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+{
+	putchar(i + '0');    /*print digits 0-9*/
+}
+
+	for (i = 0; i < 6; i++)
+{
+	putchar(i + 'a');	/*print letters a-f*/
+}
+	putchar('\n');	/*print newline character*/
+	
+	return (0);
+}
