5f1feb7f6f209cd64b7a910f1d08a9fc8f5e5f5d
root <mo.alidev369@gmail.com>
2023-02-22 17:45:15 -0800

    my commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - prints alphabet
+ * Return: void
+ */
+void print_alphabet_10x(void)
+{
+	int wheel;
+
+	for (wheel = 0; wheel <= 10; wheel++)
+{
+	print_alphabet();
+}
+}
