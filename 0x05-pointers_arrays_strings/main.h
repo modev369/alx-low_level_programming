41dd2234001b09e8cbf4bca8ac2ca63c60d3f0a2
root <mo.alidev369@gmail.com>
2023-02-28 17:27:12 -0800

    Mohamed Ali C pointers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a,int *b);
@@ -10,5 +11,6 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+void print_array(int *a, int n);
 
 #endif
