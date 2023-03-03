98dd53bb9da36729d8c6716aa431a0b4bef364c8
root <mo.alidev369@gmail.com>
2023-03-03 08:16:03 -0800

    mohamed ali signature

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: the integer to print
+ */
+void print_number(int n)
+{
+	int divisor = 1;
+
+	if (n < 0)
+	{
+	_putchar('-');
+	n = -n;
+	}
+
+	while (n / divisor >= 10)
+	{
+	divisor *= 10;
+	}
+
+	while (divisor > 0)
+	{
+	_putchar('0' + (n / divisor));
+	n %= divisor;
+	divisor /= 10;
+	}
+}
