e570335826decbc9269d7ba859b3a680a864cd90
root <mo.alidev369@gmail.com>
2023-03-03 08:23:43 -0800

    mohamed ali signature

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,29 +1,19 @@
 #include "main.h"
-#include <math.h>
 
 /**
- * print_number - prints an integer
- * @n: the integer to print
+ * print_number - prints an integer using _putchar
+ * @n: integer to print
  */
 void print_number(int n)
 {
-	int divisor = 1;
-
 	if (n < 0)
 	{
 	_putchar('-');
 	n = -n;
 	}
 
-	while (n / divisor >= 10)
-	{
-	divisor *= 10;
-	}
+	if (n / 10 != 0)
+	print_number(n / 10);
 
-	while (divisor > 0)
-	{
-	_putchar('0' + (n / divisor));
-	n %= divisor;
-	divisor /= 10;
-	}
+	_putchar(n % 10 + '0');
 }
