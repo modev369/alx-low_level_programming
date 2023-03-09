58692e3fb3c00cfc59fc0ddc54abb6993e3f4474
root <mo.alidev369@gmail.com>
2023-03-09 09:38:19 -0800

    mohamed ali

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Included libraries */
+#include <unistd.h>
+
+/* Function prototypes */
+void _puts_recursion(char *s);
+int _putchar(char c);
+
+#endif /* MAIN_H */
